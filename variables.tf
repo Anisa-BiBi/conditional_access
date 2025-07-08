8ec0b81e9ef053530df0b6ea33918e61e54d14e4
Anisa-BB <anisabibi697@gmail.com>
2025-07-08 18:39:23 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "tenant_id" {}
 variable "client_id" {}
 variable "client_secret" {}
-
-variable "trusted_location_ids" {
-  type = list(string)
-  default = []
-}
-
+variable "tenant_id" {}
