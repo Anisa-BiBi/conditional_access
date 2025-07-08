9a0318795b98995ff65cf48c972df04b9156ca60
Anisa-BB <anisabibi697@gmail.com>
2025-07-08 18:41:18 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,30 +4,46 @@ resource "azuread_conditional_access_policy" "block_untrusted_access" {
 
   conditions {
     users {
-      include_users = ["All"]
-      exclude_guests_or_external_users = true
+      included_users = ["All"]
+      excluded_users = ["GuestsOrExternalUsers"]
     }
 
     sign_in_risk_levels = ["medium", "high"]
+    user_risk_levels    = ["medium"]
+    client_app_types    = ["browser", "mobileAppsAndDesktopClients"]
 
-    locations {
-      include_location_ids = ["All"]
-      exclude_location_ids = var.trusted_location_ids
+    applications {
+      included_applications = ["All"]
     }
 
-    client_app_types = ["all"]
-    device_states {
-      include_compliant_devices_only = true
+    devices {
+      filter {
+        mode = "exclude"
+        rule = "device.operatingSystem eq \"Windows 7\""
+      }
+    }
+
+    locations {
+      included_locations = ["All"]
+      excluded_locations = ["13936a93-d6a4-47c8-b257-ef109483a433"]
+    }
+
+    platforms {
+      included_platforms = ["android", "iOS", "windows"]
     }
   }
 
   grant_controls {
-    operator         = "OR"
+    operator          = "OR"
     built_in_controls = ["mfa"]
   }
 
   session_controls {
     application_enforced_restrictions_enabled = true
+    sign_in_frequency                         = 10
+    sign_in_frequency_period                  = "hours"
+    cloud_app_security_policy                 = "monitorOnly"
+    disable_resilience_defaults               = false
   }
 }
 
