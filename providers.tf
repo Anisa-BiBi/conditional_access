c4041e4f7ca0fff8463c44d5e3e6423c5099c1d1
Anisa-BB <anisabibi697@gmail.com>
2025-07-15 14:07:21 +0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -7,6 +7,12 @@ terraform {
   }
 }
 
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
 provider "azuread" {
   tenant_id     = var.tenant_id
   client_id     = var.client_id
