47412aac8ca64c5d0053de3669c84d0dc4426894
Anisa Bibi <anisabibi697@gmail.com>
2025-07-20 16:10:49 +0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,14 +2,10 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.47"
+      version = ">= 2.43.0"
     }
   }
 }
-
 provider "azuread" {
-  tenant_id     = var.tenant_id
-  client_id     = var.client_id
-  client_secret = var.client_secret
+  use_cli = true
 }
-
